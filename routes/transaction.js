9ccf43e77af8b29de8bb3babc00ba7125e258ff5
unknown <shreyas.ny@gmail.com>
2020-09-12 12:26:46 +0530

    BackEnd Added

---
+++ routes/transaction.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const { getTransaction } = require('../controllers/transactions');
+
+
+const router = express.Router();
+
+const {getTransactions,addTransactions,deleteTransactions} = require('../controllers/transactions')
+
+router
+    .route('/')
+    .get(getTransactions)
+    .post(addTransactions);
+
+router
+    .route('/:id')
+    .delete(deleteTransactions);
+
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const morgan = require('morgan');
+const connectDB = require('./config/db')
+
+dotenv.config({path: './config/config.env'});
+connectDB();
+
+const transaction = require('./routes/transaction');
+
+const app = express();
+
+app.use(express.json());
+
+app.use('/api/v1/transactions', transaction);
+
+
+app.get('/', (req,res) => res.send('HELLO'));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
